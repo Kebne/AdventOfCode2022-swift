193357bda269799f0933ece620e3dabea1511e0f
Johan Rydenstam <johan.rydenstam@svt.se>
2022-12-03 08:56:17 +0100

    Day3 cleanup

--- Sources/AdventOfCode2022/Three.swift
+++ Sources/AdventOfCode2022/Three.swift
@@ -15,24 +15,16 @@ struct Three {
 
     func sumOfPriorities(in input: String) -> Int {
         parse(input: input)
-            .map {
-                itemsApperingTwice(in: $0)
-            }
-            .map {
-                priority(for: $0)
-            }
+            .map { itemsApperingTwice(in: $0) }
+            .map { priority(for: $0) }
             .reduce(0, +)
     }
 
     func sumOfStickers(in input: String) -> Int {
-        let sacks = parse(input: input)
-        let groups = sacks.chunked(into: 3)
-
-        return groups.map {
-            itemsApparingInAll(of: $0)
-        }.map {
-            priority(for: $0)
-        }.reduce(0, +)
+        parse(input: input)
+            .chunked(into: 3)
+            .map { itemsApparingInAll(of: $0) }
+            .map { priority(for: $0) }.reduce(0, +)
     }
 
     private func parse(input: String) -> [Rucksack] {
@@ -49,15 +41,13 @@ struct Three {
     }
 
     private func itemsApparingInAll(of rucksacks: [Rucksack]) -> Set<Character> {
-        let sackOne = rucksacks[0]
-        let sackTwo = rucksacks[1]
-        let sackThree = rucksacks[2]
+        let sackOne = rucksacks[0].0 + rucksacks[0].1
+        let sackTwo = rucksacks[1].0 + rucksacks[1].1
+        let sackThree = rucksacks[2].0 + rucksacks[2].1
 
-        let inAll = (sackOne.0 + sackOne.1).filter {
-            (sackTwo.0 + sackTwo.1).contains($0)
-        }.filter {
-            (sackThree.0 + sackThree.1).contains($0)
-        }
+        let inAll = sackOne
+            .filter { sackTwo.contains($0) }
+            .filter { sackThree.contains($0) }
 
         return Set(inAll)
     }
@@ -66,12 +56,10 @@ struct Three {
         let compA = rucksack.0
         let compB = rucksack.1
 
-        let res = compA.filter {
+        let inBoth = compA.filter {
             compB.contains($0)
         }
 
-        print("TWICE: \(res) in \(rucksack)")
-
-        return Set(res)
+        return Set(inBoth)
     }
 }
